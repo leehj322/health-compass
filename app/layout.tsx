664e273df45df974e72a660268a568759842545a
leehj322 <gudwns8956@naver.com>
2025-03-21 23:59:27 +0900

    feat(Layout): Apply Navbar component to layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "./_ui/layout/Navbar";
 
 export const metadata: Metadata = {
   title: "건강 나침반",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }

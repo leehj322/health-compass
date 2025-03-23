10b12d648678c3b751913c8a8970635d8b7379f2
leehj322 <gudwns8956@naver.com>
2025-03-23 16:28:08 +0900

    feat(mainpage): Add geolocation support logic for fetch init data

--- app/(pages)/(main)/page.tsx
+++ app/(pages)/(main)/page.tsx
@@ -1,16 +1,51 @@
 "use client";
 
-import { useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useMainPageStore } from "@/stores/useMainPageStore";
 import ScrollToTopButton from "@/app/_ui/shared/ScrollToTopButton";
 import KakaoMap from "@/app/_ui/shared/KakaoMap";
 import SearchBar from "./_ui/SearchBar";
 import NearbyFilter from "./_ui/NearbyFilter";
 import HospitalPharmacyTabs from "./_ui/HospitalPharmacyTabs";
+import { usePlacesByLocation } from "@/lib/queries/usePlaceQueries";
+
+const DEFAULT_GEOLOCATION = {
+  latitude: 37.5716486,
+  longitude: 126.97637277,
+};
 
 export default function MainPage() {
   const { activeTab, setActiveTab } = useMainPageStore();
   const topButtonTriggerRef = useRef<HTMLDivElement | null>(null);
+
+  const [geoLocation, setGeoLocation] = useState<GeolocationCoordinates | null>(
+    null,
+  );
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.log("이 브라우저는 위치 정보를 지원하지 않습니다.");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        console.log("사용자가 위치 정보를 허용했습니다.");
+        if (success.coords.accuracy < 100) {
+          setGeoLocation(success.coords);
+        } else {
+          console.log("위치 정보가 정확하지 않습니다 직접 입력 해주세요.");
+        }
+      },
+      (error) => {
+        if (error.code === error.PERMISSION_DENIED) {
+          console.log("사용자가 위치 정보 제공을 거부했습니다.");
+        } else {
+          console.log("위치 정보를 가져오는 중 오류 발생:", error.message);
+        }
+      },
+    );
+  }, []);
 
   return (
     <div className="flex flex-col md:h-[calc(100vh-5rem)] md:flex-row">

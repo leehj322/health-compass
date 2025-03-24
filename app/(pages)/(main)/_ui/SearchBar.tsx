1fff2c979f3a60841ea272a1dcc864d8fa68326b
LEE. H. J. <73635407+leehj322@users.noreply.github.com>
2025-03-25 00:24:19 +0900

    feat(mainpage): Add search functionality and map state integration (#13)
    * feat: Add keyword search api function
    
    * refactor: Abstract common logic for unifiedLocationApi.ts
    
    * feat: Integrate and add map state in zustand and add kakaomap location update logic
    
    * feat: Add search submit handler in search bar

--- app/(pages)/(main)/_ui/SearchBar.tsx
+++ app/(pages)/(main)/_ui/SearchBar.tsx
@@ -1,13 +1,44 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
+import { useState } from "react";
+import { useMainPageStore } from "@/stores/useMainPageStore";
+import { useGeoLocationStore } from "@/stores/useGeoLocation";
 
 export default function SearchBar() {
+  const [searchValue, setSearchValue] = useState("");
+  const { setFilterGroups, map } = useMainPageStore();
+  const { setGeoLocation } = useGeoLocationStore();
+
+  const handleSubmitSearchInput = (
+    e: React.MouseEvent | React.KeyboardEvent,
+  ) => {
+    const isKeyboardEvent = "key" in e;
+    if (isKeyboardEvent && e.key !== "Enter") return;
+
+    // KakaoMap ref를 통해 현재 보여지는 지도의 중앙을 현재 위치 정보로 설정하고 검색
+    if (map.ref) {
+      const center = map.ref.getCenter();
+      setGeoLocation({ lat: center.getLat(), lng: center.getLng() });
+    }
+    setFilterGroups("query", searchValue);
+  };
+
   return (
     <div className="border-b p-4">
       <div className="flex gap-2">
-        <Input placeholder="병원 또는 약국 이름 / 주소" className="flex-1" />
-        <Button variant="default" className="cursor-pointer">
+        <Input
+          value={searchValue}
+          placeholder="병원/약국에 대한 정보 (이름, 진료과 등..)을 입력하세요"
+          onChange={(e) => setSearchValue(e.target.value)}
+          onKeyDown={handleSubmitSearchInput}
+          className="flex-1"
+        />
+        <Button
+          variant="default"
+          onClick={handleSubmitSearchInput}
+          className="cursor-pointer"
+        >
           <Search className="mr-1" size={16} />
           검색
         </Button>

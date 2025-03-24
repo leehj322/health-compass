b2269e1a2c5d61cefddede72ab49170b30baa5ca
leehj322 <gudwns8956@naver.com>
2025-03-24 12:24:39 +0900

    feat: Add kakao local api address-based location search function

--- lib/api/kakaoLocal.ts
+++ lib/api/kakaoLocal.ts
@@ -36,3 +36,26 @@ export const getPlaceByCategory = async (
     );
   }
 };
+
+export const getLocationByAddress = async (search: string, page: number) => {
+  const query = `query=${search}`;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_KAKAO_LOCAL_BASE_URL}/search/address.json?${query}&page=${page}`,
+      {
+        headers: {
+          Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+        },
+      },
+    );
+
+    if (!res.ok) {
+      throw new Error(`Kakao API Error ${res.status}: ${res.text()}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw new Error(`주소 검색하기 실패: ${(error as Error).message}`);
+  }
+};

ce8933cfbbf71e77f17d3e3aff410e6b6ae4c522
leehj322 <gudwns8956@naver.com>
2025-03-26 22:33:17 +0900

    feat(lib): Add ErrorToast using shadcn sonner(toast)

---
+++ lib/toasts.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { toast } from "sonner";
+import { CircleAlert } from "lucide-react";
+
+export function ErrorToast(title: string, description?: string) {
+  toast(
+    <div>
+      <h5 className="flex items-center gap-2 font-semibold text-red-500">
+        <CircleAlert size={16} />
+        {title}
+      </h5>
+      {description && <p className="text-gray-700">{description}</p>}
+    </div>,
+  );
+}

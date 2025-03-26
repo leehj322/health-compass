05c50ad33471d2d05148862b00c6f2d2b62ce973
leehj322 <gudwns8956@naver.com>
2025-03-26 22:33:54 +0900

    refactor(hooks): Refactor useCurrentLocation hook with shadcn sonner(toast)

--- hooks/useCurrentLocation.tsx
+++ hooks/useCurrentLocation.tsx
@@ -1,32 +1,41 @@
 import { useGeoLocationStore } from "@/stores/useGeoLocation";
 import { useEffect, useState } from "react";
+import { ErrorToast } from "@/lib/toasts";
 
-export default function useCurrentLocation() {
+export function useCurrentLocation() {
   const { geoLocation, setGeoLocation } = useGeoLocationStore();
   const [isAutoLoaded, setIsAutoLoaded] = useState(false);
 
   useEffect(() => {
     if (!navigator.geolocation) {
-      console.log("이 브라우저는 위치 정보를 지원하지 않습니다.");
+      ErrorToast("이 브라우저는 위치 정보를 제공하지 않습니다.");
       return;
     }
 
     navigator.geolocation.getCurrentPosition(
       (success) => {
-        console.log("사용자가 위치 정보를 허용했습니다.");
         if (success.coords.accuracy < 100) {
           const { latitude, longitude } = success.coords;
           setGeoLocation({ lat: latitude, lng: longitude });
           setIsAutoLoaded(true);
         } else {
-          console.log("위치 정보가 정확하지 않습니다 직접 입력 해주세요.");
+          ErrorToast(
+            "위치 정보가 올바르지 않습니다.",
+            "내 주변 찾기 버튼을 눌러 등록할 수 있습니다.",
+          );
         }
       },
       (error) => {
         if (error.code === error.PERMISSION_DENIED) {
-          console.log("사용자가 위치 정보 제공을 거부했습니다.");
+          ErrorToast(
+            "위치 정보 제공을 승인하지 않았습니다.",
+            "내 주변 찾기 버튼을 눌러 등록할 수 있습니다.",
+          );
         } else {
-          console.log("위치 정보를 가져오는 중 오류 발생:", error.message);
+          ErrorToast(
+            "위치 정보 설정 실패",
+            "내 주변 찾기 버튼을 눌러 등록할 수 있습니다.",
+          );
         }
       },
     );

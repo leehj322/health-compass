ece3aa8c758170f81d444bde06d87532c61ffd4b
LEE. H. J. <73635407+leehj322@users.noreply.github.com>
2025-03-29 02:00:51 +0900

    feat(auth): Add `supabase` related libraries and api routes (#30)
    * chore: Add `supabase/supabase-js` and `supabase/ssr`
    
    * feat: Add supabase create client function for browser and server environment
    
    * feat: Setup middleware for auth
    
    * feat: Add supabase-powered api routes (login, logout, signup)
    
    * fix: Fix lint error `@typescript-eslint/no-unused-vars`

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const supabase = await createClient();
+
+  const { email, password } = await req.json();
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return NextResponse.json(
+      { success: false, message: error.message },
+      { status: 401 },
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+  });
+}

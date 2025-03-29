fdd2b6ee0f5002e5cf90752b974924da8856a851
leehj322 <gudwns8956@naver.com>
2025-03-30 03:24:59 +0900

    feat: Improve signin error handling with custom codes and messages

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -5,19 +5,45 @@ export async function POST(req: Request) {
   const supabase = await createClient();
 
   const { email, password } = await req.json();
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
 
-  if (error) {
+  try {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (!error) return NextResponse.json({ success: true });
+
+    // UNKNOWN_ERROR: 기본 에러 코드 및 메시지 (아래 조건에 해당하지 않는 경우)
+    let code = "UNKNOWN_ERROR";
+    let message = error.message;
+
+    // INVALID_CREDENTIALS: 이메일 혹은 비밀번호가 유효하지 않은 경우
+    // 회원가입을 하지 않았거나, 이메일 또는 비밀번호가 잘못된 경우 발생
+    if (error.message.includes("Invalid login credentials")) {
+      code = "INVALID_CREDENTIALS";
+      message = "이메일이나 비밀번호가 잘못되었습니다.";
+    }
+
+    // EMAIL_NOT_CONFIRMED: 이메일이 아직 인증되지 않은 경우
+    // 회원가입은 하였으나 이메일 인증을 하지 않은 경우 발생
+    if (error.message.includes("Email not confirmed")) {
+      code = "EMAIL_NOT_CONFIRMED";
+      message = "이메일 인증이 완료되지 않았습니다.";
+    }
+
     return NextResponse.json(
-      { success: false, message: error.message },
-      { status: 401 },
+      { success: false, code, message },
+      { status: 400 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        code: "SERVER_ERROR",
+        message: "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+      },
+      { status: 500 },
     );
   }
-
-  return NextResponse.json({
-    success: true,
-  });
 }

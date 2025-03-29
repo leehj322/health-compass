65c4fab69046158b47a197a120a0e6a27c9ae443
leehj322 <gudwns8956@naver.com>
2025-03-29 17:49:22 +0900

    feat: Move signup page route and add UI

---
+++ app/(users)/auth/signup/page.tsx
@@ -0,0 +1,21 @@
+import AuthTitle from "../_ui/AuthTitle";
+import SignUpForm from "./_ui/SignUpForm";
+import FormDivider from "../_ui/FormDivider";
+import SocialSignUpButtons from "./_ui/SocialSignUpButtons";
+import AuthPageRedirectLink from "../_ui/AuthRedirectLink";
+
+export default function SignUpPage() {
+  return (
+    <div className="mt-15 flex flex-col items-center justify-center bg-white px-4 sm:mt-23 sm:px-6 md:px-8 lg:mt-30">
+      <AuthTitle />
+      <SignUpForm />
+
+      <FormDivider />
+
+      <SocialSignUpButtons />
+      <AuthPageRedirectLink href="/auth/signin">
+        이미 회원이신가요?
+      </AuthPageRedirectLink>
+    </div>
+  );
+}

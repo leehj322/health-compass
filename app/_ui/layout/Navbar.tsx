0edca9e18bcce124383d934ab767d162433b16bb
leehj322 <gudwns8956@naver.com>
2025-03-30 18:48:09 +0900

    refactor: Simplify menu structure and add links
    - Add link to "/" at logo
    - Add bell icon button for notifications
    - Use map() to render nav menu items from static list

--- app/_ui/layout/Navbar.tsx
+++ app/_ui/layout/Navbar.tsx
@@ -1,31 +1,44 @@
 import Link from "next/link";
-import { LogIn, CircleHelp } from "lucide-react";
-import MobileMenu from "./MobileMenu";
-import DesktopMenu from "./DesktopMenu";
+import NavMenu from "./NavMenu";
+import { LogIn, CircleHelp, Bell } from "lucide-react";
+
+const MENU_LIST = [
+  { label: "병원 약국 찾기", href: "/", highlightPaths: ["/clinic"] },
+  { label: "응급실 정보", href: "/emergency", highlightPaths: [] },
+  { label: "자유게시판", href: "/board", highlightPaths: [] },
+];
 
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 flex h-20 items-center justify-between bg-white px-6 py-6 shadow-sm md:px-20">
       <div className="text-xl font-bold md:text-2xl">
-        <span className="text-emerald-600">건강</span> 나침반
+        <Link href="/">
+          <span className="text-emerald-600">건강</span> 나침반
+        </Link>
       </div>
       <ul className="flex items-center gap-4 font-semibold md:gap-6">
-        {/* 데스크탑 메뉴 */}
-        <div className="hidden items-center gap-6 md:flex">
-          <DesktopMenu />
-        </div>
-
-        {/* 모바일 메뉴 버튼 */}
-        <li className="flex items-center justify-center md:hidden">
-          <MobileMenu />
-        </li>
+        {/* 메뉴 리스트 (모바일의 경우 드롭다운) */}
+        <NavMenu menuList={MENU_LIST} />
 
         {/* 아이콘 형태 메뉴 (데스크탑, 모바일 동일) */}
         <li className="flex gap-3">
-          <Link href="/help" aria-label="문의하기">
+          <button
+            className="hover:cursor-pointer"
+            title="알림"
+            aria-label="알림"
+          >
+            <Bell size={20} />
+          </button>
+
+          <Link
+            href="/help"
+            title="문의하기"
+            aria-label="문의하기"
+            className="flex items-center justify-center"
+          >
             <CircleHelp size={20} />
           </Link>
-          <Link href="/login" aria-label="로그인">
+          <Link href="/auth/signin" title="로그인" aria-label="로그인">
             <LogIn size={20} />
           </Link>
         </li>

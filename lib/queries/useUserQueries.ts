dd9f295771a06d9d4f8c815d590bbb1a86c01d3c
leehj322 <gudwns8956@naver.com>
2025-03-31 01:30:01 +0900

    feat: Fetch basic user info from supabase

---
+++ lib/queries/useUserQueries.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "./queryKeys";
+
+export const useUser = () => {
+  return useQuery({
+    queryKey: QUERY_KEYS.user.all,
+    queryFn: async () => {
+      const res = await fetch("api/user");
+      const data = await res.json();
+      return data.user;
+    },
+  });
+};

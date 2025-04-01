b06749329a4bc4bac5dc4c456f3c1b23c29ce2fc
leehj322 <gudwns8956@naver.com>
2025-04-01 18:34:41 +0900

    feat: Define comment types and sync with SQL schema

--- lib/api/comments/comments.type.ts
+++ lib/api/comments/comments.type.ts
@@ -1,9 +1,15 @@
+import { UserProfile } from "../user/user.type";
+
+// DetailComment: 상세 페이지 댓글
+
+// Mutation에 사용 (useCreateComment)
 export interface DetailCommentInput {
   content: string;
   external_institution_id: string;
   parent_id?: string | null;
 }
 
+// DB에 저장되는 Comment 타입
 export interface DetailComment {
   id: string;
   content: string;
@@ -14,9 +20,24 @@ export interface DetailComment {
   external_institution_id: string;
 }
 
-export interface CommentPostResponse {
-  comment: Comment;
+export interface DetailCommentWithProfile extends DetailComment {
+  profiles: UserProfile;
+}
+
+// 상위 댓글
+export interface TopLevelDetailComment extends DetailCommentWithProfile {
+  parent_id: null;
+  children: ReplyDetailComment[];
+}
+
+// 답글
+export interface ReplyDetailComment extends DetailCommentWithProfile {
+  parent_id: string;
+  children?: never; // 없음
+}
+
+export interface CommentListResponse {
   success: boolean;
-  code?: string;
-  message?: string;
+  comments: TopLevelDetailComment[];
+  page: number;
 }

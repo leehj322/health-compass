3c68bf2247d3c529489873fecc3f17f89350864f
leehj322 <gudwns8956@naver.com>
2025-04-02 18:04:35 +0900

    feat(detailpage): Add scrollToTopButton to detail page

--- app/(pages)/clinic/[id]/_ui/CommentForm.tsx
+++ app/(pages)/clinic/[id]/_ui/CommentForm.tsx
@@ -4,14 +4,16 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { useCreateDetailComment } from "@/lib/queries/useCommentsQueries";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { ErrorToast } from "@/lib/toasts";
 import { useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/lib/queries/queryKeys";
 import Spinner from "@/app/_ui/shared/Spinner";
+import ScrollToTopButton from "@/app/_ui/shared/ScrollToTopButton";
 
 export default function CommentForm() {
+  const formRef = useRef<HTMLDivElement | null>(null);
   const [content, setContent] = useState("");
   const pathname = usePathname();
   const queryClient = useQueryClient();
@@ -38,24 +40,28 @@ export default function CommentForm() {
   };
 
   return (
-    <Card className="border-emerald-600 py-0">
-      <CardContent className="space-y-2 p-4">
-        <p className="font-semibold text-emerald-600">댓글 작성</p>
-        <Textarea
-          placeholder="댓글을 입력하세요..."
-          className="resize-none overflow-hidden text-sm"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          disabled={isPending}
-        />
-        <Button
-          onClick={handleCreateButtonClick}
-          disabled={isPending || !content.trim()}
-          className="ml-auto flex w-24 cursor-pointer bg-emerald-600 text-white hover:bg-emerald-700"
-        >
-          {isPending ? <Spinner /> : "등록"}
-        </Button>
-      </CardContent>
-    </Card>
+    <>
+      <Card className="border-emerald-600 py-0" ref={formRef}>
+        <CardContent className="space-y-2 p-4">
+          <p className="font-semibold text-emerald-600">댓글 작성</p>
+          <Textarea
+            placeholder="댓글을 입력하세요..."
+            className="resize-none overflow-hidden text-sm"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            disabled={isPending}
+          />
+          <Button
+            onClick={handleCreateButtonClick}
+            disabled={isPending || !content.trim()}
+            className="ml-auto flex w-24 cursor-pointer bg-emerald-600 text-white hover:bg-emerald-700"
+          >
+            {isPending ? <Spinner /> : "등록"}
+          </Button>
+        </CardContent>
+      </Card>
+
+      <ScrollToTopButton triggerRef={formRef} />
+    </>
   );
 }

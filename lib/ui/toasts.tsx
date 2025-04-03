ea6ad36fefa34527516b5fb3ffe487edfebaa175
leehj322 <gudwns8956@naver.com>
2025-04-03 19:30:15 +0900

    refactor: Move toast.tsx and convert regular functions to arrow functions.

--- lib/ui/toasts.tsx
+++ lib/ui/toasts.tsx
@@ -3,16 +3,16 @@
 import { toast } from "sonner";
 import { CircleAlert } from "lucide-react";
 
-export function defaultToast(title: string, description?: string) {
+export const defaultToast = (title: string, description?: string) => {
   toast(
     <div>
       <h5 className="flex items-center gap-2 font-semibold">{title}</h5>
       {description && <p className="text-gray-700">{description}</p>}
     </div>,
   );
-}
+};
 
-export function ErrorToast(title: string, description?: string) {
+export const errorToast = (title: string, description?: string) => {
   toast(
     <div>
       <h5 className="flex items-center gap-2 font-semibold text-red-500">
@@ -22,4 +22,4 @@ export function ErrorToast(title: string, description?: string) {
       {description && <p className="text-gray-700">{description}</p>}
     </div>,
   );
-}
+};
